35bd7e763b0b2ad4ac9de17802c4f62a46b200fd
kauanguilhermesantos <kauansantos1411@outlook.com>
2025-02-15 17:34:05 -0300

    debugando Medico

---
+++ src/Medico.java
@@ -0,0 +1,34 @@
+package src;
+
+import java.util.Date;
+
+public class Medico extends Usuario {
+    private String areaEspecializada;
+    private String crm;
+    
+    public Medico(String nome, String cpf, Date dataNascimento, String telefone, String areaEspecializada, String crm){
+        super(nome, cpf, dataNascimento, telefone);
+        this.areaEspecializada = areaEspecializada;
+        this.crm = crm;
+    }
+
+    public String getEspecialidade(){
+        return areaEspecializada;
+    }
+    public String getCrm(){
+        return crm;
+    }
+
+    public void setEspecialidade(String areaEspecializada){
+        this.areaEspecializada = areaEspecializada;
+    }
+
+    public void setCrm(String crm){
+        this.crm = crm;
+    }
+
+    void atender(){
+        System.out.println(String.format("O medico %s esta atendendo, aguarde o seu horario" , getNome()));
+    }
+
+}
