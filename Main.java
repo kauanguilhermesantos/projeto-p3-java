2994927d76662f8f77a93ac52796ce90b6df09ae
kauanguilhermesantos <kauansantos1411@outlook.com>
2025-02-17 10:40:33 -0300

    atualizando Main

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
+import javax.swing.SwingUtilities;
+import src.ui.TelaPrincipal;
+
 public class Main {
-    public static void main(String[] args) {
-        
-    }
+   public static void main(String[] var0) {
+      SwingUtilities.invokeLater(() -> {
+        (new TelaPrincipal()).mostrarTela();
+      });
+   }
 }
